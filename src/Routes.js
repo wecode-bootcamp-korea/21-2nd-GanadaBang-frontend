13f032491a30db59df27113a370ba058e20eddd3
UlongChaS2 <jyujung2@gmail.com>
2021-06-22 20:06:13 +0900

    Add: 레이아웃, styled component, url에 따라 다른 style Modify: code review feedback, menu 타겟팅 되었을 때 다른 style Modify: styled component

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Nav from './Components/Nav/Nav';
 import Main from './Pages/Main/Main';
 import Product from './Pages/Product/Product';
 
 function Routes() {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
-        <Route exact path="/main" component={Main} />
         <Route exact path="/product" component={Product} />
+        <Route exact path="/" component={Main} />
       </Switch>
     </BrowserRouter>
   );
