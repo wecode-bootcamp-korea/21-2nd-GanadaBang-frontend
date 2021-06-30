29988bfbc8e167c90f174c924e10c2c921af7b54
UlongChaS2 <jyujung2@gmail.com>
2021-07-01 00:34:00 +0900

    로그아웃 구현중 '컴플릭트' 해결하려 '브랜치' 바꿈 최종 업로드! 였으면

--- src/Routes.js
+++ src/Routes.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
 import Main from './Pages/Main/Main';
+import SaleRoom from './Pages/SaleRoom/SaleRoom';
 import Product from './Pages/Product/Product';
 import MapPage from './Pages/Map/MapPage';
+import LoginModal from './Components/LoginModal/LoginModal';
 
 function Routes() {
   return (
@@ -12,9 +14,11 @@ function Routes() {
       <Nav />
       <Switch>
         <Route exact path="/rooms/:id" component={Product} />
-        <Route exact path="/" component={Main} />
-        <Route exact path="/main" component={Main} />
+        <Route exact path="/product" component={Product} />
+        <Route exact path="/login" component={LoginModal} />
+        <Route exact path="/saleroom" component={SaleRoom} />
         <Route exact path="/map" component={MapPage} />
+        <Route exact path="/" component={Main} />
       </Switch>
       <Footer />
     </BrowserRouter>
